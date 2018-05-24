da8805b78504c253aafd14800c61861169b722d9
alteest <alteest@gmail.com>
2018-05-24 21:32:13 +0500

    Table review

--- src/com/imagesectors/DisplayImage.java
+++ src/com/imagesectors/DisplayImage.java
@@ -6,7 +6,6 @@ import java.awt.EventQueue;
 import java.awt.FlowLayout;
 
 import javax.swing.ImageIcon;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -32,13 +31,9 @@ public class DisplayImage extends JFrame {
 
     private void initUI() {       
         
-        ImageIcon ii = loadImage();
+        createLayout();
 
-        JLabel label = new JLabel(ii);
-
-        createLayout(label);
-
-        setSize(700,  700);
+        setSize(1500,  900);
         setTitle("Image");
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -50,45 +45,35 @@ public class DisplayImage extends JFrame {
         return ii;
     }
 
-    private void createLayout(JComponent... arg) {
+    private void createLayout() {
     	setLayout(new FlowLayout());
     	
-        //Container pane = getContentPane();
-        //GroupLayout gl = new GroupLayout(pane);
-        //pane.setLayout(gl);
-
         sectors = new SectorList();
         sectors.readFromCSV("src/data.csv");
         data = sectors.convert2Data();
-        tableModel = new DefaultTableModel(data, columnNames); 
-        table = new JTable(tableModel); 
+        tableModel = new DefaultTableModel(data, columnNames);
+        table = new JTable(tableModel);
+        table.getColumnModel().getColumn(0).setPreferredWidth(240);
+        table.getColumnModel().getColumn(1).setPreferredWidth(40);
+        table.getColumnModel().getColumn(2).setPreferredWidth(40);
+        table.getColumnModel().getColumn(3).setPreferredWidth(40);
+        table.getColumnModel().getColumn(4).setPreferredWidth(40);
+        table.getColumnModel().getColumn(5).setPreferredWidth(100);
         table.setAutoCreateRowSorter(true);
         JScrollPane scrollPane = new JScrollPane(table); 
-        scrollPane.setPreferredSize(new Dimension(380,280)); 
+        scrollPane.setPreferredSize(new Dimension(500, 800)); 
         JPanel outerPanel = new JPanel(new BorderLayout());
         JPanel panel = new JPanel(new BorderLayout()); 
-        panel.add(scrollPane, BorderLayout.BEFORE_LINE_BEGINS);
-        panel.add(arg[0], BorderLayout.CENTER);
+        panel.add(scrollPane, BorderLayout.WEST);
+        
+        ImageIcon ii = loadImage();
+        JLabel label = new JLabel(ii);
+        label.setPreferredSize(new Dimension(800, 800));
+        panel.add(label, BorderLayout.CENTER);
+
         outerPanel.add(panel, BorderLayout.BEFORE_FIRST_LINE);
         
         add(outerPanel);
-        
-        //gl.setAutoCreateContainerGaps(true);
-        //gl.setHorizontalGroup(gl.createSequentialGroup()
-        //        .addComponent(panel)
-        //);
-        //gl.setHorizontalGroup(gl.createSequentialGroup()
-        //        .addComponent(arg[0])
-        //);
-        //gl.setVerticalGroup(gl.createParallelGroup()
-        //        .addComponent(panel)
-        //);
-
-        //gl.setVerticalGroup(gl.createParallelGroup()
-        //        .addComponent(arg[0])
-        //);
-
-        //pack();
     }
 
     public static void main(String[] args) {
